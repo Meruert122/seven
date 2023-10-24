eb7f9b4ac642d26304a0778b3bd6f7ea509fbdb8
meruert.alizhan@bk.ru <113488815+Meruert122@users.noreply.github.com>
2023-10-24 10:16:17 +0600

    first commit

---
+++ seven/lib/generated/codegen_loader.g.dart
@@ -0,0 +1,135 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: prefer_single_quotes
+
+import 'dart:ui';
+
+import 'package:easy_localization/easy_localization.dart' show AssetLoader;
+
+class CodegenLoader extends AssetLoader{
+  const CodegenLoader();
+
+  @override
+  Future<Map<String, dynamic>?> load(String path, Locale locale) {
+    return Future.value(mapLocales[locale.toString()]);
+  }
+
+  static const Map<String,dynamic> en = {
+  "hello": "Submit",
+  "register_form": "Registration Page",
+  "full_name": "Complete Name",
+  "what_do_people_call_you": "What's your nickname?",
+  "phone_number": "Contact Number",
+  "where_can_we_reach_you": "Provide your contact",
+  "enter_phone_format": "Format: (XXX)XXX-XXXX",
+  "phone_number_format_hint": "Format should be (###)###-####",
+  "email_address": "Email ID",
+  "enter_email_address": "Provide your email",
+  "country": "Nation",
+  "life_story": "Biography",
+  "tell_us_about_yourself": "Brief about you",
+  "keep_it_short": "Brief version, as it's demo",
+  "password": "Passcode",
+  "enter_password": "Fill your passcode",
+  "confirm_password": "Re-enter Passcode",
+  "confirm_password_hint": "Reconfirm your passcode",
+  "form_not_valid": "Check the form! Something's off",
+  "name_required": "We need your name.",
+  "enter_alphabetical_characters": "Alphabets only, please.",
+  "email_empty": "Where's the email?",
+  "invalid_email": "That doesn't look right.",
+  "name": "Username",
+  "phone": "Mobile",
+  "email": "Electronic Mail",
+  "eight_character_required": "We need 8 chars for the passcode",
+  "password_mismatch": "Uh-oh, passcodes aren't matching",
+  "registration_successful": "Done and dusted!",
+  "verified_register_form": "is now certified",
+  "verified": "Confirmed",
+  "user_info": "User Details",
+  "not_specified": "Undefined",
+  "russia": "Russian Federation",
+  "ukraine": "Ukrainian State",
+  "germany": "German Republic",
+  "france": "French Territory"
+};
+static const Map<String,dynamic> kk = {
+  "hello": "Жіберу",
+  "register_form": "Тіркеу парағы",
+  "full_name": "Толық аты-жөні",
+  "what_do_people_call_you": "Сізді қалай шақырады?",
+  "phone_number": "Телефон номері",
+  "where_can_we_reach_you": "Сізбен қалай байланысуға болады?",
+  "enter_phone_format": "Форматы: (XXX)XXX-XXXX",
+  "phone_number_format_hint": "Формат (###)###-#### болуы керек",
+  "email_address": "Электрондық пошта",
+  "enter_email_address": "Электрондық поштаны енгізіңіз",
+  "country": "Ел",
+  "life_story": "Өмір тарихы",
+  "tell_us_about_yourself": "Өзіңіз туралы айтып беріңіз",
+  "keep_it_short": "Қысқаша сипаттаңыз, бұл демо версия",
+  "password": "Құпия сөз",
+  "enter_password": "Құпия сөзді енгізіңіз",
+  "confirm_password": "Құпия сөзді растаңыз",
+  "confirm_password_hint": "Құпия сөзді қайталаңыз",
+  "form_not_valid": "Форма дұрыс толтырылмаған! Тексеріп, түзету қажет",
+  "name_required": "Аты-жөні көрсету қажет",
+  "enter_alphabetical_characters": "Тек әріптерді енгізіңіз",
+  "email_empty": "Электрондық пошта бос",
+  "invalid_email": "Электрондық пошта дұрыс емес",
+  "name": "Аты-жөні",
+  "phone": "Телефон",
+  "email": "Электрондық пошта",
+  "eight_character_required": "Құпия сөз 8 таңбадан тұратын болуы керек",
+  "password_mismatch": "Құпия сөздер сәйкес келмейді",
+  "registration_successful": "Тіркеу сәтті аяқталды",
+  "verified_register_form": "тіркеу формасы расталған",
+  "verified": "Тексерілген",
+  "user_info": "Пайдаланушы ақпараты",
+  "not_specified": "Көрсетілмеген",
+  "russia": "Орысия",
+  "ukraine": "Украина",
+  "germany": "Германия",
+  "france": "Франция"
+};
+static const Map<String,dynamic> ru = {
+  "hello": "Отправить",
+  "register_form": "Страница Регистрации",
+  "full_name": "Полное Имя",
+  "what_do_people_call_you": "Как вас называют?",
+  "phone_number": "Номер Телефона",
+  "where_can_we_reach_you": "Ваш контакт",
+  "enter_phone_format": "Формат: (XXX)XXX-XXXX",
+  "phone_number_format_hint": "Формат должен быть (###)###-####",
+  "email_address": "Email ID",
+  "enter_email_address": "Укажите ваш email",
+  "country": "Страна",
+  "life_story": "Биография",
+  "tell_us_about_yourself": "Кратко о себе",
+  "keep_it_short": "Краткая версия, это демо",
+  "password": "Пароль",
+  "enter_password": "Введите пароль",
+  "confirm_password": "Повторите Пароль",
+  "confirm_password_hint": "Подтвердите ваш пароль",
+  "form_not_valid": "Проверьте форму! Что-то не так",
+  "name_required": "Нам нужно ваше имя.",
+  "enter_alphabetical_characters": "Только буквы.",
+  "email_empty": "Где email?",
+  "invalid_email": "Это выглядит неправильно.",
+  "name": "Имя Пользователя",
+  "phone": "Мобильный",
+  "email": "Электронная Почта",
+  "eight_character_required": "Нам нужно 8 символов для пароля",
+  "password_mismatch": "Пароли не совпадают",
+  "registration_successful": "Завершено!",
+  "verified_register_form": "теперь сертифицирован",
+  "verified": "Подтверждено",
+  "user_info": "Детали Пользователя",
+  "not_specified": "Не указано",
+  "russia": "Российская Федерация",
+  "ukraine": "Украинское Государство",
+  "germany": "Германская Республика",
+  "france": "Французская Территория"
+};
+static const Map<String, Map<String,dynamic>> mapLocales = {"en": en, "kk": kk, "ru": ru};
+}
